4d110a497f9f6d41791dbeb021e91c2e49b3f1ab
Raisa_Dev <73198136+raisa-design@users.noreply.github.com>
2024-10-12 10:52:38 -0300

    refactor/add-mvc edit mvc login users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -9,74 +9,82 @@ class UserController {
   async register(req, res) {
     const { nome, email, senha, role } = req.body;
 
-  if (!nome || !email || !senha || !role) {
-    return res
-      .status(400)
-      .json({ mensagem: "Todos os campos são obrigatórios" });
-  }
-
-  // Verifica se o role é válido
-  if (!["admin", "editor", "cliente"].includes(role)) {
-    return res.status(400).json({ mensagem: "Função inválida" });
-  }
-
-  try {
-    const userModel = new UserModel();
-    // Verificar se o email já está em uso
-    if(await userModel.userExists(email)){
-      return res.status(400).json({ mensagem: "Email já cadastrado" });
+    if (!nome || !email || !senha || !role) {
+      return res
+        .status(400)
+        .json({ mensagem: "Todos os campos são obrigatórios" });
     }
 
-    // Hash da senha
-    const saltRounds = 10;
-    const hashedSenha = await bcrypt.hash(senha, saltRounds);
+    // Verifica se o role é válido
+    if (!["admin", "editor", "cliente"].includes(role)) {
+      return res.status(400).json({ mensagem: "Função inválida" });
+    }
 
-    // Inserir o novo usuário no banco de dados
-    const novoUsuario = await userModel.addUser(email, nome, hashedSenha, role);
+    try {
+      const userModel = new UserModel();
+      // Verificar se o email já está em uso
+      if (await userModel.userExists(email)) {
+        return res.status(400).json({ mensagem: "Email já cadastrado" });
+      }
 
-    // Gerar token JWT para o novo usuário
-    const user = {
-      id: novoUsuario.id,
-      email: novoUsuario.email,
-      role: novoUsuario.role,
-    };
-    const accessToken = jwt.sign(user, JWT_SECRET, { expiresIn: "1h" });
+      // Hash da senha
+      const saltRounds = 10;
+      const hashedSenha = await bcrypt.hash(senha, saltRounds);
 
-    return res.status(201).json({ accessToken });
-  } catch (error) {
-    console.error("Erro ao registrar usuário:", error);
-    return res.status(500).json({ mensagem: "Erro ao registrar usuário" });
+      // Inserir o novo usuário no banco de dados
+      const novoUsuario = await userModel.addUser(
+        email,
+        nome,
+        hashedSenha,
+        role
+      );
+
+      // Gerar token JWT para o novo usuário
+      const user = {
+        id: novoUsuario.id,
+        email: novoUsuario.email,
+        role: novoUsuario.role,
+      };
+      const accessToken = jwt.sign(user, JWT_SECRET, { expiresIn: "1h" });
+
+      return res.status(201).json({ accessToken });
+    } catch (error) {
+      console.error("Erro ao registrar usuário:", error);
+      return res.status(500).json({ mensagem: "Erro ao registrar usuário" });
+    }
   }
-  }
-  
-    async login(req, res) {
-        
-        try {
-            const { email, senha } = req.body;
 
-            if (!email || !senha) {
-              return res.status(400).json({ mensagem: "Email e senha são obrigatórios" });
-            }    
-        
-            // Verificar se o email e senha estão corretos
-            const usuario = await userModel.userLogin(email, senha);
-            if (!usuario) {
-                return res.status(400).json({ mensagem: "Email ou senha incorretos" });
-            }
-        
-            // Gerar token JWT
-            const user = {
-                id: usuario.id,
-                email: usuario.email,
-                role: usuario.role,
-            };
-        
-            const accessToken = jwt.sign(user, JWT_SECRET, { expiresIn: "1h" });
-            return res.json({ accessToken });
-        } catch (error) {
-            console.error("Erro ao fazer login:", error);
-            return res.status(500).json({ mensagem: "Erro ao fazer login" });
-        }
-        }   
+  async login(req, res) {
+    try {
+      const { email, senha } = req.body;
+
+      if (!email || !senha) {
+        // TODO: Validar os campos com ZOD
+        return res
+          .status(400)
+          .json({ mensagem: "Email e senha são obrigatórios" });
+      }
+
+      // Verificar se o email e senha estão corretos
+      const usuario = await userModel.findUserByEmail(email);
+      const senhaValida = await bcrypt.compare(senha, usuario?.senha || "");
+      if (!usuario || !senhaValida) {
+        return res.status(401).json({ mensagem: "Email ou senha incorretos" });
+      }
+
+      // Gerar token JWT
+      const user = {
+        id: usuario.id,
+        email: usuario.email,
+        role: usuario.role,
+      };
+
+      const accessToken = jwt.sign(user, JWT_SECRET, { expiresIn: "1h" });
+      return res.json({ accessToken });
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
+      return res.status(500).json({ mensagem: "Erro ao fazer login" });
+    }
+  }
 }
 module.exports = new UserController();
--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,42 +1,35 @@
 const pool = require("../database/connection");
-const bcrypt = require("bcrypt");
 
 class UserModel {
-    async addUser(email, nome, hashedSenha, role) {
-        const queryText = `INSERT INTO users (nome, email, senha, role) VALUES ($1, $2, $3, $4) RETURNING *`;
-        const queryValues = [nome, email, hashedSenha, role];
-        const novoUsuario = await pool.query(queryText, queryValues);
-        return novoUsuario.rows[0];
+  async addUser(email, nome, hashedSenha, role) {
+    const queryText = `INSERT INTO users (nome, email, senha, role) VALUES ($1, $2, $3, $4) RETURNING *`;
+    const queryValues = [nome, email, hashedSenha, role];
+    const novoUsuario = await pool.query(queryText, queryValues);
+    return novoUsuario.rows[0];
+  }
+
+  async userExists(email) {
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    if (result.rows.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  async findUserByEmail(email) {
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (result.rows.length === 0) {
+      return null; // Retorna null se o usuário não for encontrado
     }
 
-    async userExists(email) {
-        const result = await pool.query("SELECT * FROM users WHERE email = $1", [
-            email,
-        ]);
-        if (result.rows.length > 0) {
-            return true;
-        }else{
-            return false;
-        }
-    }
-    async userLogin(email, senha) {
-        const result = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+    const usuario = result.rows[0];
 
-        if (result.rows.length === 0) {
-            return null; // Retorna null se o usuário não for encontrado
-        }
-
-        const usuario = result.rows[0];
-
-        // Verificar a senha
-        const senhaValida = await bcrypt.compare(senha, usuario.senha);
-
-        if (!senhaValida) {
-            return null; // Retorna null se a senha for inválida
-        }
-
-        return usuario;
-    }
-    
+    return usuario;
+  }
 }
 module.exports = UserModel;
