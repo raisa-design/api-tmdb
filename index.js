52b44c7e0caced069f0550526e7131d8f8695baa
Raisa_Dev <73198136+raisa-design@users.noreply.github.com>
2024-10-13 12:08:18 -0300

    feat: add doc swagger

--- index.js
+++ index.js
@@ -1,105 +1,13 @@
-const express = require("express");
-const axios = require("axios");
-const { z } = require("zod");
+const app = require("express")();
+const http = require("http");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const pool = require("./src/database/connection");
-const server = express();
-const cheerio = require("cheerio");
-const request = require("request");
-const UserController = require("./src/controllers/UserController");
-const MovieController = require("./src/controllers/MovieController");
-const ScrapperController = require("./src/controllers/ScrapperController");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger_output.json");
 
-const TMDB_API_KEY = "79b3ceee03442ea90980fe372e0b8fdc";
-const JWT_SECRET = "your_jwt_secret_key"; // Substitua por uma chave secreta forte
+http.createServer(app).listen(3000);
 
-server.use(express.json());
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-// Middleware para verificar o token JWT
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+require("./endpoints")(app);
 
-  if (!token) {
-    return res.status(401).json({ mensagem: "Token não fornecido" });
-  }
-
-  jwt.verify(token, JWT_SECRET, (err, user) => {
-    if (err) {
-      return res.status(403).json({ mensagem: "Token inválido" });
-    }
-
-    req.user = user;
-    next();
-  });
-};
-
-// Rota para registrar um novo usuário
-server.post("/register", async (req, res) => UserController.register(req, res));
-
-// Rota de login para gerar o token JWT
-server.post("/login", async (req, res) => UserController.login(req, res));
-
-// Middleware para verificar permissões
-const authorize = (roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).json({ mensagem: "Permissão negada" });
-    }
-    next();
-  };
-};
-
-server.get("/", (req, res) => {
-  return res.json({ mensagem: "Hello World" });
-});
-
-// Rota protegida
-
-server.post(
-  "/filmes",
-  authenticateToken,
-  authorize(["admin"]),
-  async (req, res) => MovieController.createFilme(req, res)
-);
-
-// Rota protegida para obter detalhes de um filme
-server.get(
-  "/filmes/:id",
-  authenticateToken,
-  authorize(["admin", "editor", "cliente"]),
-  async (req, res) => {
-    MovieController.getMovieId(req, res);
-  }
-);
-
-// Rota protegida para editar um filme (admin e editor podem editar)
-server.put(
-  "/filmes/:id",
-  authenticateToken,
-  authorize(["admin", "editor"]),
-  async (req, res) => {
-    MovieController.updateMovie(req, res);
-  }
-);
-
-// Rota protegida para obter filmes com paginação
-server.get(
-  "/filmes",
-  authenticateToken,
-  authorize(["admin", "editor", "cliente"]),
-  async (req, res) => {
-    MovieController.getMovieAll(req, res);
-  }
-);
-// Rota para scrapping
-server.post("/scrapper", async (req, res) => {
-  ScrapperController.scrapperNews(req, res);
-});
-
-server.listen(3000, () => {
-  console.log("Servidor está funcionando...");
-});
-
-module.exports = server;
+module.exports = app;
